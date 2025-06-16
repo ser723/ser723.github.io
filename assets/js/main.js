5097e93e82c52389b51cade45d585fda179788a7
Sarah Robinson <saraherobinson723@gmail.com>
2025-06-16 11:47:57 -0400

    Added my portfolio files

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function ($) {
 
 
 		/*======= Populate GitHub Projects *=======*/
-		let gitHubUsername = 'YOUR GITHUB USERNAME';
+		let gitHubUsername = 'ser723';
 
 		// Run GitHub API function, passing in the GitHub username
 		requestUserRepos(gitHubUsername)
